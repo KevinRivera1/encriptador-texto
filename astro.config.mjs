67c049bd05ab46956fba97774c4c1b3c574caa1b
Kevin Rivera <shimpo11.kr@gmail.com>
2024-09-14 16:59:17 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://kevinrivera1.github.io/",
+    base: "encriptador-texto"
+});
