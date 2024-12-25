df99d776906f6a8938f9ebc0ea12809923141ac7
Kevin Rivera <shimpo11.kr@gmail.com>
2024-12-25 15:28:58 -0500

    feat: agregar configuración de Vite para PostCSS en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from 'astro/config';
+import {
+    defineConfig
+} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://kevinrivera1.github.io/",
-    base: "encriptador-texto"
+    base: "encriptador-texto",
+    vite: {
+        css: {
+            transformer: 'postcss'
+        }
+    }
 });
